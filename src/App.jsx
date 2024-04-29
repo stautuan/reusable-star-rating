db640665e42f790c84e34a6ec0ed020b6e9ad91d
stautuan <stautuan89@gmail.com>
2024-04-29 19:49:01 -0400

    Generated the number of stars using Array.from()

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-
 function App() {
-  
+  return (
+    <div>
+      <div>
+        {/* Generating the # of stars */}
+        {Array.from({ length: 5 }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p>10</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
