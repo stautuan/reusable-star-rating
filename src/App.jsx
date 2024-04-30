98168ac536cc1e5953d0ea90364d3e692d561ee5
stautuan <stautuan89@gmail.com>
2024-04-29 20:29:05 -0400

    Created a temporary rating state when the user hovers over the stars using onMouseEnter and onMouseLeave events

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ const textStyle = {
 
 function App({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -31,10 +32,12 @@ function App({ maxRating = 5 }) {
             key={i}
             onRate={() => handleRating(i + 1)}
             full={rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ''}</p>
+      <p style={textStyle}>{tempRating || ''}</p>
     </div>
   );
 }
@@ -46,9 +49,15 @@ const starStyle = {
   cursor: 'pointer',
 };
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role='button' style={starStyle} onClick={onRate}>
+    <span
+      role='button'
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns='http://www.w3.org/2000/svg'
